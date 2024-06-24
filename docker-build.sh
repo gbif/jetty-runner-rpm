88b432ac61b26a133984760ac96fc7288682a1c1
Matthew Blissett <mblissett@gbif.org>
2024-06-24 16:57:04 +0200

    Build using Oracle Linux.

--- docker-build.sh
+++ docker-build.sh
@@ -14,25 +14,25 @@ mkdir -p $CURRENT_DIR/{RPMS,SOURCES}
 chmod 777 $CURRENT_DIR/{RPMS,SOURCES}
 
 echo
-echo "CentOS Stream8 build"
-docker pull quay.io/centos/centos:stream8
+echo "EL8 build using Oracle Linux"
+docker pull oraclelinux:8
 docker run --rm \
        -e nr_ver=$nr_ver \
        -v $CURRENT_DIR/RPMS:/root/rpmbuild/RPMS/ \
        -v $CURRENT_DIR/SOURCES:/root/rpmbuild/SOURCES/ \
        -v $CURRENT_DIR/SPECS:/root/rpmbuild/SPECS/ \
-       quay.io/centos/centos:stream8 \
+       oraclelinux:8 \
        "/root/rpmbuild/SPECS/rpm-build.sh"
 
 echo
-echo "CentOS Stream9 build"
-docker pull quay.io/centos/centos:stream9
+echo "EL9 build using Oracle Linux"
+docker pull oraclelinux:9
 docker run --rm \
        -e nr_ver=$nr_ver \
        -v $CURRENT_DIR/RPMS:/root/rpmbuild/RPMS/ \
        -v $CURRENT_DIR/SOURCES:/root/rpmbuild/SOURCES/ \
        -v $CURRENT_DIR/SPECS:/root/rpmbuild/SPECS/ \
-       quay.io/centos/centos:stream9 \
+       oraclelinux:9 \
        "/root/rpmbuild/SPECS/rpm-build.sh"
 
 chmod 755 $CURRENT_DIR/{RPMS,SOURCES}
