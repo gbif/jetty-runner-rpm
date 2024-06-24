66b45f4b190adc794fd620f7d57dd7dbce5602c1
Matthew Blissett <mblissett@gbif.org>
2024-06-24 11:32:09 +0200

    Revert "Remove EL8 build"
    This reverts commit baf7dddc6e08d9ecc2073216923b3b45e0b303c5.

--- docker-build.sh
+++ docker-build.sh
@@ -3,7 +3,7 @@
 # Check if the package version is up-to-date
 nr_ver=$(curl -Ss 'https://search.maven.org/solrsearch/select?q=g:org.eclipse.jetty+a:jetty-runner+v:10*&rows=20&wt=json' | jq -r '.response.docs[0].v')
 
-if curl -I --fail https://packages.gbif.org/el9/rpm/jetty-runner-$nr_ver-1.el9.noarch.rpm &> /dev/null; then
+if curl -I --fail https://packages.gbif.org/el8/rpm/jetty-runner-$nr_ver-1.el8.noarch.rpm &> /dev/null; then
     echo "Version $nr_ver exists already, nothing to do"
     exit 0
 fi
@@ -12,6 +12,17 @@ CURRENT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 
 mkdir -p $CURRENT_DIR/{RPMS,SOURCES}
 chmod 777 $CURRENT_DIR/{RPMS,SOURCES}
+
+echo
+echo "CentOS Stream8 build"
+docker pull quay.io/centos/centos:stream8
+docker run --rm \
+       -e nr_ver=$nr_ver \
+       -v $CURRENT_DIR/RPMS:/root/rpmbuild/RPMS/ \
+       -v $CURRENT_DIR/SOURCES:/root/rpmbuild/SOURCES/ \
+       -v $CURRENT_DIR/SPECS:/root/rpmbuild/SPECS/ \
+       quay.io/centos/centos:stream8 \
+       "/root/rpmbuild/SPECS/rpm-build.sh"
 
 echo
 echo "CentOS Stream9 build"
